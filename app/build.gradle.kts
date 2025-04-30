7697b4a6b3a23820883f5979cc3e3974709aed47
Alexandre Aguiar <161641176+picks1307@users.noreply.github.com>
2025-04-30 10:47:40 -0300

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+// build.gradle.kts (raiz do projeto)
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.9.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
---
+++ app/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "AgendaCorteApp"
+include(":app")
